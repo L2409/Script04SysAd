f619f3dc19596600b313d0c605af5c76ee7a5487
Logan <ljd2409@g.rit.edu>
2021-11-06 16:33:37 -0400

    added stuff

--- attacker_report.py
+++ attacker_report.py
@@ -1,1 +1,38 @@
-#hello
+#!/usr/bin/python3
+#Logan Dorman
+#11/6/21
+
+from geoip import geolite2
+import os
+from datetime import date
+
+def main():
+    os.system("clear")
+    day = date.today()
+    filename = "FILENAME"
+    keyword = "Failed password"
+    ip_dict = {}
+    print("Attacker Report -",day,"\n")
+
+    with open(filename) as file:
+        file = file.readlines()
+    
+    for line in file:
+        if keyword in line:
+            tokens = line.split(" ")
+            ip = tokens[11]
+            try:
+                ip_dict[ip] += 1
+            except:
+                ip_dict[ip] = 0
+                print("NEW IP! ",ip)
+
+    for entry in ip_dict:
+        print("COUNT: ",ip_dict[entry], "IP ADDR: ", ip_dict[entry], "COUNTRY: ", "WIP", sep="")
+    
+
+
+if __name__ == "__main__":
+    main()
+
+#Info on how to get date from: https://www.programiz.com/python-programming/datetime/current-datetime
